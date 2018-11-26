aacf0d404156cef8784e122b7b1be65c34f15239
Malinovskaya Ekaterina <Katerina@Air-on-the-Air-2.local>
2018-11-27 00:35:10 +0300

    init: Add first task

--- 1.py
+++ 1.py
@@ -1,25 +1,20 @@
-# Логин должен начинаться с латинской буквы, он может состоять из латинских букв,
-# цифр, точки и минуса и должен заканчиваться латинской буквой или цифрой.
-# Минимальная длина логина — 1 символ. Максимальная — 20 символов».
-# Напишите код, проверяющий соответствие входной строки этому правилу.
-# На выходе должен получиться скрипт или бинарник с regexp'ом и тестами.
-
 import re
 
+
 def checking(login):
-    if re.match(r'^[a-z]{1}[a-z0-9-.]{0,18}[a-z0-9]{1}$', login, re.IGNORECASE):
-        print('true')
+    if re.match(r'^[a-z]{1}[a-z0-9-.]{0,18}[a-z0-9]?$', login, re.IGNORECASE):
         return True
     else:
-        print('false')
         return False
 
+
 if __name__ == '__main__':
-    checking('kjfhsjkfhskjfd')
-    checking('')
-    checking('_')
-    checking('x244345.jdfhsjf')
-    checking('a1234567890098765432')
-    checking('qwerty.')
-    checking('a1234567899876543210z')
-    checking('df-dfd.dfd111FJHF')
+    import random
+    import string
+
+    BASE_STRING = string.ascii_letters + string.digits + string.punctuation
+    sample = ''.join([random.choice(BASE_STRING) for _ in range(random.randint(0, 25))])
+    if checking(login=sample):
+        print(f'{sample} is valid')
+    else:
+        print(f'{sample} is invalid')
